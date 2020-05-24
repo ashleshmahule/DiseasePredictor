a464745e2ebd768bf23119fd220aad7db73e6d34
ashleshmahule <ashleshmahule15@gmail.com>
2020-05-24 23:38:00 +0530

    react app added

--- disease-prediction-app/src/App.js
+++ disease-prediction-app/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.css';
-import background from './heartbeat.jpg';
+import Background from './heartbeat.jpg';
 import GetDisease from './GetDisease';
+import FormClass from './FormClass';
 
 
 function App() {
   
+
   
   return (
-    <div className="App" style = {{ backgroundImage: "url(" + background + ")"}}>
+    <div className="App">
       <header className="App-header">
         <h1>Welcome</h1>
-        <GetDisease symp='cough'/>
+        
       </header>
+      <FormClass />
+        {/* <GetDisease symp='cough'/> */}
     </div>
   );
 }
