cb92923bb6b2ce9c1170d41f6242c397d7a34b00
ashleshmahule <ashleshmahule15@gmail.com>
2020-05-25 16:44:03 +0530

    updation

--- disease-prediction-app/src/Home.js
+++ disease-prediction-app/src/Home.js
@@ -5,26 +5,49 @@ import 'react-bootstrap-typeahead/css/Typeahead.css';
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import { Link } from "react-router-dom";
 
-function Home() {
-    return (
-        <>
-            <NavbarHead />
-            <header className="App-header">
-                <h1>Welcome</h1>
-            </header>
-            <Form id="form">
-                <Row className="justify-content-md-center">
-                    <Col sm="3" md="3" lg="3" >
-                        <Form.Group as={Form.Row} controlId="name">
-                            <Form.Control type="text" placeholder="Enter Your Name" />
-                        </Form.Group>
-                    </Col>
-                </Row>
-            </Form>
-        </>
-    );
+
+
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: null
+        }
+    }
+
+    handleChange = event => {
+        this.setState({ username: event.target.value });
+    }
+
+    render() {
+        return (
+            <>
+                <NavbarHead />
+                <header className="App-header">
+                    <h1>Welcome {this.state.username}</h1>
+                </header>
+
+                <Form id="form">
+                    <Row className="justify-content-md-center">
+                        <Col sm="3" md="3" lg="3" >
+                            <Form.Group as={Form.Row} controlId="name">
+                                <Form.Control onChange={this.handleChange} type="text" placeholder="Enter Your Name" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                </Form>
+
+
+                <Link to='/DiseasePrediction'>
+                    <Button className="gs" variant="light">Get Started</Button>
+                </Link>
+            </>
+        );
+    }
 }
 
 export default Home;
