5a28dba7eee57eecd4ef0dc9467f6c36ec20dd79
ashleshmahule <ashleshmahule15@gmail.com>
2020-05-27 17:08:13 +0530

    .

--- server.js
+++ server.js
@@ -10,17 +10,19 @@ var cors = require('cors');
 cors({credentials: true, origin: true})
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/ShowMore"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(compression());
 app.use(express.static(path.resolve(__dirname, "disease-prediction-app/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "disease-prediction-app/build", "index.html"));
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/ShowMore"); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 
 
